7a95f57dbe17773b243f8d3c5d749df37dbe31a7
luispipe <lflondonor@unal.edu.co>
2023-11-01 23:12:54 -0300

    Four commit

--- app/src/main/java/com/example/example_model_04/MainActivity.java
+++ app/src/main/java/com/example/example_model_04/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.example_model_04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -75,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
                 } else{
                     Student studentObject= new Student(code,name,emailStudent,phoneStudent);
                     studentsList.put(code,studentObject);
+                    System.out.println(code);
                     limpiar();
                 }
 
@@ -107,6 +109,10 @@ public class MainActivity extends AppCompatActivity {
                        double finalGrade= Double.parseDouble(gradeObtain);
                        Grades gradeObject= new Grades(codeGrade,codeInt,courseName,teacherName,dateGrade,finalGrade);
                        gradesList.put(codeGrade,gradeObject);
+                        Toast.makeText(getApplicationContext(),
+                                "Se registro el usuario",
+                                Toast.LENGTH_LONG).show();
+                       limpiarRegistro();
                     }else{
                         Toast.makeText(getApplicationContext(),
                         "El estudiante no se encuentra en la base de datos",
@@ -126,6 +132,7 @@ public class MainActivity extends AppCompatActivity {
                 }else{
                     int codeInt= Integer.parseInt(code);
                     if (validateUser(codeInt,codesList)){
+                        table.removeAllViews();
                         showResults(codeInt,gradesList,studentsList);
                     }else{
                         Toast.makeText(getApplicationContext(),
@@ -167,25 +174,90 @@ public class MainActivity extends AppCompatActivity {
         email.setText("");
     }
 
+    public void limpiarRegistro(){
+        codeStudent.setText("");
+        course.setText("");
+        teacher.setText("");
+        grade.setText("");
+    }
+
+
+
 
     public void showResults(int code,HashMap<String,Grades>gradesList,HashMap<Integer,Student>studentList){
-   //     HashMap<String,Grades> newList= new HashMap<>();
+       /*   StudentList
+       *    {123698={123698,"Valentina","123646","valentina@gmail.com"},
+       *     741852={741852,"Erick","310258639","erick@gmail.com"},
+       *     ...
+       *     456987={456987,"Diana","32569878","diana@gmail.com"}
+       *    }
+       *
+       *    GradeList
+       *    { "lu23sk23"={"lu23sk23",123698,"APPS","Luis","2023-11-01",5},
+       *      "ka52los4"={"ka52los4",123698,"Base de datos","Luis","2023-10-29",4.5},
+       *       ...
+       *      "ilpow41"={"ilpow41",741852,"APPS","Luis","2023-11-01",4}
+               }
+       *
+       * */
+
+        // El usuario quiere consultar las notas del estudiantes con código: 123698
+
+        //381
+        TableRow row1= new TableRow(this);
+        TextView nameTitle= new TextView(this);
+        nameTitle.setText("Estudiante");
+        nameTitle.setWidth(140);
+        nameTitle.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        nameTitle.setBackgroundColor(Color.CYAN);
+
+        TextView courseTitle= new TextView(this);
+        courseTitle.setText("Curso");
+        courseTitle.setWidth(130);
+        courseTitle.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        courseTitle.setBackgroundColor(Color.CYAN);
+
+        TextView gradeTitle= new TextView(this);
+        gradeTitle.setText("Nota");
+        gradeTitle.setWidth(80);
+        gradeTitle.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        gradeTitle.setBackgroundColor(Color.CYAN);
+
+        TextView dateTitle= new TextView(this);
+        dateTitle.setText("Fecha");
+        dateTitle.setWidth(80);
+        dateTitle.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        dateTitle.setBackgroundColor(Color.CYAN);
+
+        row1.addView(nameTitle);
+        row1.addView(courseTitle);
+        row1.addView(gradeTitle);
+        row1.addView(dateTitle);
+
+        table.addView(row1);
 
         for (String i:gradesList.keySet()){
             if(gradesList.get(i).getStudent_code()==code){
             //    newList.put(i,gradesList.get(i));
                 TableRow row= new TableRow(this);
+
                 TextView name= new TextView(this);
-                name.setText(studentList.get(code).getName());
+                Student object= studentList.get(code);
+                name.setText(object.getName());
+                name.setWidth(140);
+                // name.setText(studentList.get(code).get(name));
 
                 TextView courseName= new TextView(this);
                 courseName.setText(gradesList.get(i).getCourse());
+                courseName.setWidth(140);
 
                 TextView finalGrade= new TextView(this);
                 finalGrade.setText(gradesList.get(i).getGrade()+"");
+                finalGrade.setWidth(80);
 
                 TextView date= new TextView(this);
                 date.setText(gradesList.get(i).getDate());
+                date.setWidth(80);
 
                 row.addView(name);
                 row.addView(courseName);
